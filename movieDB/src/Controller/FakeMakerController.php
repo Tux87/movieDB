fb282c65862522342bdfd2e6d37efd6d62c3ce9b
Tux87 <edward.juniper87@gmail.com>
2021-09-28 01:54:53 +0200

    Integration Index, Fake Maker BoB OK

---
+++ movieDB/src/Controller/FakeMakerController.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Character;
+use App\Entity\Episode;
+use App\Entity\Season;
+use App\Entity\TvShow;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FakeMakerController extends AbstractController
+{
+    /**
+     * @Route("/fake/data/got/", name="fake_data_got")
+     */
+    public function got(EntityManagerInterface $entityManager): Response
+    {
+
+        // créer un  show
+        $got = new TvShow();
+        $got->setTitle('Game of Thrones');
+        $got->setSynopsis('Game of Thrones - il fait froid et plein de gens font pleins de trucs, Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam, debitis atque perferendis neque natus optio minus. Nihil temporibus velit in nam at blanditiis quisquam eligendi nisi aspernatur magni, dolore, reprehenderit ad enim architecto atque fugiat libero, quae eos beatae? Aliquid iure, ullam vitae eaque deleniti quidem quod fugit dignissimos et? Voluptatem voluptates tempore sapiente hic suscipit eveniet excepturi, iure dicta. Maiores animi nostrum iste nulla placeat exercitationem, dolorum quasi magnam magni incidunt cumque voluptate eaque quaerat, dolor consequatur sit voluptas, officiis fuga voluptatem deleniti blanditiis amet? Rem quo, qui sed numquam ex similique aperiam harum cum deleniti commodi officia sit.');
+        $got->setImage('https://fr.web.img6.acsta.net/pictures/19/03/21/17/05/1927893.jpg');
+        $got->setNbLikes(mt_rand(1, 1000));
+        $got->setCreatedAt(new DateTimeImmutable);
+        // les valeurs suivantes sont définies en valeur par défaut
+        // $got->setCreatedAt(new DateTimeImmutable());
+        // $got->setNbLikes(0);
+       
+
+        $entityManager->persist($got);
+
+
+        // créer des saisons
+        $year = 2010;
+        for ($seasonNumber = 1; $seasonNumber <= 7; $seasonNumber++) {
+            $season = new Season();
+            $seasonYear = $year + $seasonNumber;
+            $season->setPublishedAt(new DateTimeImmutable($seasonYear . '-01-01'));
+            $season->setSeasonNumber($seasonNumber);
+            // associer les saisons au show
+            $season->setTvShow($got);
+
+            $entityManager->persist($season);
+
+            // pour la saison actuelle, on crée des épisodes
+            for ($episodeNumber = 1; $episodeNumber <= 9; $episodeNumber++) {
+
+                $episode = new Episode();
+                $episode->setEpisodeNumber($episodeNumber);
+                $episode->setTitle('S0' . $seasonNumber . 'x0' . $episodeNumber);
+                // associer les épisodes aux saisons
+                $episode->setSeason($season);
+                $episode->setCreatedAt(new DateTimeImmutable);
+
+
+                $entityManager->persist($episode);
+            }
+        }
+
+        // enregistrer le tout en BDD
+        $entityManager->flush();
+
+
+        return $this->render('fake_data_maker/index.html.twig', [
+            'controller_name' => 'FakeDataMakerController',
+        ]);
+    }
+
+    /**
+     * @Route("/fake/data/mrRobot/", name="fake_data_robot")
+     */
+    public function mrRobot(EntityManagerInterface $entityManager): Response
+    {
+
+        // créer un  show
+        $tvShow = new TvShow();
+        $tvShow->setTitle('Mr Robot');
+        $tvShow->setSynopsis('Hacking++ , Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam, debitis atque perferendis neque natus optio minus. Nihil temporibus velit in nam at blanditiis quisquam eligendi nisi aspernatur magni, dolore, reprehenderit ad enim architecto atque fugiat libero, quae eos beatae? Aliquid iure, ullam vitae eaque deleniti quidem quod fugit dignissimos et? Voluptatem voluptates tempore sapiente hic suscipit eveniet excepturi, iure dicta. Maiores animi nostrum iste nulla placeat exercitationem, dolorum quasi magnam magni incidunt cumque voluptate eaque quaerat, dolor consequatur sit voluptas, officiis fuga voluptatem deleniti blanditiis amet? Rem quo, qui sed numquam ex similique aperiam harum cum deleniti commodi officia sit.');
+        $tvShow->setImage('https://fr.web.img3.acsta.net/pictures/15/05/06/16/13/020977.jpg');
+        $tvShow->setCreatedAt(new DateTimeImmutable);
+        $tvShow->setNbLikes(mt_rand(1, 1000));
+        // les valeurs suivantes sont définies en valeur par défaut
+        // $tvShow->setCreatedAt(new DateTimeImmutable());
+        // $tvShow->setNbLikes(0);
+       
+
+        $entityManager->persist($tvShow);
+
+
+        // créer des saisons
+        $year = 2010;
+        for ($seasonNumber = 1; $seasonNumber <= 4; $seasonNumber++) {
+            $season = new Season();
+            $seasonYear = $year + $seasonNumber;
+            $season->setPublishedAt(new DateTimeImmutable($seasonYear . '-01-01'));
+            $season->setSeasonNumber($seasonNumber);
+            // associer les saisons au show
+            $season->setTvShow($tvShow);
+
+            $entityManager->persist($season);
+
+            // pour la saison actuelle, on crée des épisodes
+            for ($episodeNumber = 1; $episodeNumber <= 10; $episodeNumber++) {
+
+                $episode = new Episode();
+                $episode->setEpisodeNumber($episodeNumber);
+                $episodeTitle = $tvShow->getTitle() . ' - S0' . $seasonNumber . 'x0' . $episodeNumber;
+                if ($episodeNumber >= 10) {
+                    $episodeTitle = $tvShow->getTitle() . ' - S0' . $seasonNumber . 'x' . $episodeNumber;
+                }
+                $episode->setTitle($episodeTitle);
+                // associer les épisodes aux saisons
+                $episode->setSeason($season);
+                $episode->setCreatedAt(new DateTimeImmutable);
+
+
+                $entityManager->persist($episode);
+            }
+        }
+
+        // enregistrer le tout en BDD
+        $entityManager->flush();
+
+
+        return $this->render('fake_data_maker/index.html.twig', [
+            'controller_name' => 'FakeDataMakerController',
+        ]);
+    }
+
+    /**
+     * @Route("/fake/data/bob/", name="fake_data_bob")
+     */
+    public function bob(EntityManagerInterface $entityManager): Response
+    {
+
+        // créer un  show
+        $year = 2001;
+        $bob = new TvShow();
+        $bob->setTitle('Band of Brothers');
+        $bob->setSynopsis('The series dramatizes the history of "Easy" Company, 2nd Battalion, 506th Parachute Infantry Regiment, of the 101st Airborne Division, from jump training in the United States through its participation in major actions in Europe, up until Japan\'s capitulation and the end of World War II.');
+        $bob->setImage('https://fr.web.img2.acsta.net/pictures/21/02/11/14/18/3309028.jpg');
+        $bob->setNbLikes(mt_rand(1, 1000));
+        $bob->setCreatedAt(new DateTimeImmutable($year . '-09-09'));
+        // les valeurs suivantes sont définies en valeur par défaut
+        // $tvShow->setCreatedAt(new DateTimeImmutable());
+        // $tvShow->setNbLikes(0);
+        $entityManager->persist($bob);
+
+
+        // créer des saisons
+        $seasonNumber = 1;
+        
+        $season = new Season();
+        $seasonYear = $year + $seasonNumber;
+        $season->setPublishedAt(new DateTimeImmutable($seasonYear . '-09-09'));
+        $season->setSeasonNumber($seasonNumber);
+        $season->setCreatedAt(new DateTimeImmutable($seasonYear . '-09-09'));
+        // associer les saisons au show
+        $season->setTvShow($bob);
+
+        $entityManager->persist($season);
+
+        // pour la saison actuelle, on crée des épisodes
+        for ($episodeNumber = 1; $episodeNumber <= 10; $episodeNumber++) {
+            $episode = new Episode();
+            $episode->setEpisodeNumber($episodeNumber);
+            $episodeTitle = $bob->getTitle() . ' - S0' . $seasonNumber . 'x0' . $episodeNumber;
+            if ($episodeNumber >= 10) {
+                $episodeTitle = $bob->getTitle() . ' - S0' . $seasonNumber . 'x' . $episodeNumber;
+            }
+            $episode->setTitle($episodeTitle);
+            // associer les épisodes aux saisons
+            $episode->setSeason($season);
+            $episode->setCreatedAt(new DateTimeImmutable);
+
+
+            $entityManager->persist($episode);
+        }
+
+        $character = new Character();
+        $character->setFirstName('Richard');
+        $character->setLastName('Winters');
+        $character->setGender('Male');
+        $character->setBio('Richard Davis "Dick" Winters (January 21, 1918 – January 2, 2011) was an officer of the United States Army and a decorated war veteran. He is best known for having commanded Easy Company of the 2nd Battalion, 506th Parachute Infantry Regiment, part of the 101st Airborne Division, during World War II. He was eventually promoted to major and put in command of the 2nd Battalion.');
+        $character->setAge('26');
+        $character->setCreatedAt(new DateTimeImmutable);
+        $entityManager->persist($character);
+
+        $character = new Character();
+        $character->setFirstName('Darrell ');
+        $character->setLastName('Powers');
+        $character->setGender('Male');
+        $character->setBio('Staff Sergeant Darrell Cecil "Shifty" Powers (13 March 1923 – 17 June 2009)[1] was a non-commissioned officer with Easy Company, 2nd Battalion, 506th Parachute Infantry Regiment, in the 101st Airborne Division during World War II. Powers was portrayed in the HBO miniseries Band of Brothers by Peter Youngblood Hills.');
+        $character->setAge('21');
+        $character->setCreatedAt(new DateTimeImmutable);
+
+        $entityManager->persist($character);
+
+
+        $character = new Character();
+        $character->setFirstName('Carwood');
+        $character->setLastName('Lipton');
+        $character->setGender('Male');
+        $character->setBio('Clifford Carwood Lipton (30 January 1920 – 16 December 2001)[1] was a commissioned officer with Easy Company, 506th Parachute Infantry Regiment, 101st Airborne Division, during World War II.');
+        $character->setAge('22');
+        $character->setCreatedAt(new DateTimeImmutable);
+
+        $entityManager->persist($character);
+        
+
+        // enregistrer le tout en BDD
+        $entityManager->flush();
+
+        return $this->redirectToRoute('tv_show_index');
+    }
+}
--- movieDB/var/cache/dev/url_matching_routes.php
+++ movieDB/var/cache/dev/url_matching_routes.php
@@ -13,11 +13,19 @@ return [
         '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
         '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
         '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
-        '/category' => [[['_route' => 'category', '_controller' => 'App\\Controller\\CategoryController::index'], null, null, null, false, false, null]],
-        '/character' => [[['_route' => 'character', '_controller' => 'App\\Controller\\CharacterController::index'], null, null, null, false, false, null]],
-        '/episode' => [[['_route' => 'episode', '_controller' => 'App\\Controller\\EpisodeController::index'], null, null, null, false, false, null]],
-        '/season' => [[['_route' => 'season', '_controller' => 'App\\Controller\\SeasonController::index'], null, null, null, false, false, null]],
-        '/tv/show' => [[['_route' => 'tv_show', '_controller' => 'App\\Controller\\TvShowController::index'], null, null, null, false, false, null]],
+        '/category' => [[['_route' => 'category_index', '_controller' => 'App\\Controller\\CategoryController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/category/new' => [[['_route' => 'category_new', '_controller' => 'App\\Controller\\CategoryController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/character' => [[['_route' => 'character_index', '_controller' => 'App\\Controller\\CharacterController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/character/new' => [[['_route' => 'character_new', '_controller' => 'App\\Controller\\CharacterController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/episode' => [[['_route' => 'episode_index', '_controller' => 'App\\Controller\\EpisodeController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/episode/new' => [[['_route' => 'episode_new', '_controller' => 'App\\Controller\\EpisodeController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/fake/data/got' => [[['_route' => 'fake_data_got', '_controller' => 'App\\Controller\\FakeMakerController::got'], null, null, null, true, false, null]],
+        '/fake/data/mrRobot' => [[['_route' => 'fake_data_robot', '_controller' => 'App\\Controller\\FakeMakerController::mrRobot'], null, null, null, true, false, null]],
+        '/fake/data/bob' => [[['_route' => 'fake_data_bob', '_controller' => 'App\\Controller\\FakeMakerController::bob'], null, null, null, true, false, null]],
+        '/season' => [[['_route' => 'season_index', '_controller' => 'App\\Controller\\SeasonController::index'], null, ['GET' => 0], null, true, false, null]],
+        '/season/new' => [[['_route' => 'season_new', '_controller' => 'App\\Controller\\SeasonController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        '/' => [[['_route' => 'tv_show_index', '_controller' => 'App\\Controller\\TvShowController::index'], null, ['GET' => 0], null, false, false, null]],
+        '/new' => [[['_route' => 'tv_show_new', '_controller' => 'App\\Controller\\TvShowController::new'], null, ['GET' => 0, 'POST' => 1], null, false, false, null]],
     ],
     [ // $regexpList
         0 => '{^(?'
@@ -34,8 +42,35 @@ return [
                         .')'
                         .'|(*:124)'
                     .')'
-                    .'|error/(\\d+)(?:\\.([^/]++))?(*:159)'
                 .')'
+                .'|/c(?'
+                    .'|ategory/([^/]++)(?'
+                        .'|(*:158)'
+                        .'|/edit(*:171)'
+                        .'|(*:179)'
+                    .')'
+                    .'|haracter/([^/]++)(?'
+                        .'|(*:208)'
+                        .'|/edit(*:221)'
+                        .'|(*:229)'
+                    .')'
+                .')'
+                .'|/episode/([^/]++)(?'
+                    .'|(*:259)'
+                    .'|/edit(*:272)'
+                    .'|(*:280)'
+                .')'
+                .'|/season/([^/]++)(?'
+                    .'|(*:308)'
+                    .'|/edit(*:321)'
+                    .'|(*:329)'
+                .')'
+                .'|/([^/]++)(?'
+                    .'|(*:350)'
+                    .'|/edit(*:363)'
+                    .'|(*:371)'
+                .')'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:408)'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
@@ -45,7 +80,22 @@ return [
         101 => [[['_route' => '_profiler_exception', '_controller' => 'web_profiler.controller.exception_panel::body'], ['token'], null, null, false, false, null]],
         114 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception_panel::stylesheet'], ['token'], null, null, false, false, null]],
         124 => [[['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null]],
-        159 => [
+        158 => [[['_route' => 'category_show', '_controller' => 'App\\Controller\\CategoryController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        171 => [[['_route' => 'category_edit', '_controller' => 'App\\Controller\\CategoryController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        179 => [[['_route' => 'category_delete', '_controller' => 'App\\Controller\\CategoryController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        208 => [[['_route' => 'character_show', '_controller' => 'App\\Controller\\CharacterController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        221 => [[['_route' => 'character_edit', '_controller' => 'App\\Controller\\CharacterController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        229 => [[['_route' => 'character_delete', '_controller' => 'App\\Controller\\CharacterController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        259 => [[['_route' => 'episode_show', '_controller' => 'App\\Controller\\EpisodeController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        272 => [[['_route' => 'episode_edit', '_controller' => 'App\\Controller\\EpisodeController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        280 => [[['_route' => 'episode_delete', '_controller' => 'App\\Controller\\EpisodeController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        308 => [[['_route' => 'season_show', '_controller' => 'App\\Controller\\SeasonController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        321 => [[['_route' => 'season_edit', '_controller' => 'App\\Controller\\SeasonController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        329 => [[['_route' => 'season_delete', '_controller' => 'App\\Controller\\SeasonController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        350 => [[['_route' => 'tv_show_show', '_controller' => 'App\\Controller\\TvShowController::show'], ['id'], ['GET' => 0], null, false, true, null]],
+        363 => [[['_route' => 'tv_show_edit', '_controller' => 'App\\Controller\\TvShowController::edit'], ['id'], ['GET' => 0, 'POST' => 1], null, false, false, null]],
+        371 => [[['_route' => 'tv_show_delete', '_controller' => 'App\\Controller\\TvShowController::delete'], ['id'], ['POST' => 0], null, false, true, null]],
+        408 => [
             [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],

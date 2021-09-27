d2fed3229eb323765c6e63e804605d89ed2bb574
Tux87 <edward.juniper87@gmail.com>
2021-09-28 01:06:35 +0200

    Creation et Migration de BDD OK

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ba84b2aa9329b626aa3acf1240e124d9e57731d5',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ba84b2aa9329b626aa3acf1240e124d9e57731d5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
